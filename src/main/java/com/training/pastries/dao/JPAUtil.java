1a8ca07f31dd339d21f1bcf38c6a68f5a35f904a
Maxence <maxence.tutois@outlook.fr>
2023-01-26 09:30:29 +0100

    Update JPAUtil.java

--- src/main/java/com/training/pastries/dao/JPAUtil.java
+++ src/main/java/com/training/pastries/dao/JPAUtil.java
@@ -3,9 +3,11 @@ package com.training.pastries.dao;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
-public class JPAUtil {
+public final class JPAUtil {
     private static final String PERSISTENCE_UNIT_NAME = "PU";
     public static EntityManagerFactory factory;
+
+    private JPAUtil() {}
 
     public static EntityManagerFactory getEntityManagerFactory() {
         if (factory == null)
